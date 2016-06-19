27d31d362555b27329bb4ceb59c52fff5bccc9b5
saquib <saquib@pressfriendly.com>
2016-06-19 20:32:02 -0300

    final edits

--- app/controllers/postgresql/schemas_controller.rb
+++ app/controllers/postgresql/schemas_controller.rb
@@ -1,7 +1,17 @@
 class PostgreSQL::SchemasController < PostgreSQL::BaseController
 
   def index
-    render locals: {schemas: current_database.schemas} rescue render_404
+    begin
+      schemas = current_database.schemas
+      if schemas.size == 1
+        redirect_to controller: :tables,
+                    schema_name: schemas[0].name
+      else
+        render locals: {schemas: schemas}
+      end
+    rescue 
+      render_404
+    end
   end
 
 end
